6128784abc9513efb5b5a6eac13df865d1101284
Machine-builder <37518116+Machine-builder@users.noreply.github.com>
2018-09-07 08:10:00 +1000

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,15 @@
 import urllib.request
-import json
+import json, shutil
+
+PYTHONEXTENSION = ".py3"
 
 def downloadGitFile(filename, repo = "https://raw.githubusercontent.com/Machine-builder/script-auto-updater/master/"):
     response = urllib.request.urlopen(str(repo)+str(filename))
     html = response.read().decode()
+
+    if filename.endswith(".py"):
+        filename = filename.replace(".py", PYTHONEXTENSION, 1)
+
     open("downloads\\"+filename,"w").write(html)
 
 downloadGitFile("data.json")
@@ -20,3 +26,7 @@ if not newver > curver:
 else:
     print("There's an update we need")
     input("press enter to download the update ...")
+    downloadGitFile("script.py")
+    LOCATION = "downloads\\%s" % ( "script.py".replace(".py",PYTHONEXTENSION))
+    DESTINATION = "script.py".replace(".py",PYTHONEXTENSION)
+    shutil.move( LOCATION , DESTINATION )
